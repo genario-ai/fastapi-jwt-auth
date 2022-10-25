b193df99d9296a8e2cc782fc5710438308c24b25
benjamin <benjamin@genario.co>
2022-10-25 09:00:34 +0200

    fix: update delete_cookie function

--- fastapi_jwt_auth/auth_jwt.py
+++ fastapi_jwt_auth/auth_jwt.py
@@ -447,14 +447,20 @@ class AuthJWT(AuthConfig):
         response.delete_cookie(
             self._access_cookie_key,
             path=self._access_cookie_path,
-            domain=self._cookie_domain
+            domain=self._cookie_domain,
+            secure=self._cookie_secure,
+            httponly=True,
+            samesite=self._cookie_samesite
         )
 
         if self._cookie_csrf_protect:
             response.delete_cookie(
                 self._access_csrf_cookie_key,
                 path=self._access_csrf_cookie_path,
-                domain=self._cookie_domain
+                domain=self._cookie_domain,
+                secure=self._cookie_secure,
+                httponly=False,
+                samesite=self._cookie_samesite
             )
 
     def unset_refresh_cookies(self,response: Optional[Response] = None) -> None:
@@ -476,14 +482,20 @@ class AuthJWT(AuthConfig):
         response.delete_cookie(
             self._refresh_cookie_key,
             path=self._refresh_cookie_path,
-            domain=self._cookie_domain
+            domain=self._cookie_domain,
+            secure=self._cookie_secure,
+            httponly=True,
+            samesite=self._cookie_samesite
         )
 
         if self._cookie_csrf_protect:
             response.delete_cookie(
                 self._refresh_csrf_cookie_key,
                 path=self._refresh_csrf_cookie_path,
-                domain=self._cookie_domain
+                domain=self._cookie_domain,
+                secure=self._cookie_secure,
+                httponly=False,
+                samesite=self._cookie_samesite
             )
 
     async def _verify_and_get_jwt_optional_in_cookies(
